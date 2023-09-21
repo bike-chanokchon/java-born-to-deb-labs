1659d6155ec2c7ff50d466f5cfec0631b2e2ce86
Colorfuls <bike.chanokchon2001@gmail.com>
2023-09-21 18:16:29 +0700

    Update GradCal.java

--- 1_star/GradCal.java
+++ 1_star/GradCal.java
@@ -10,16 +10,22 @@ public class GradCal {
         int score = sc.nextInt();
         sc.close();
 
-        if (score >= 80) {
-            System.out.println("A");
-        } else if (score < 80 && score >= 70) {
-            System.out.println("B");
-        } else if (score < 70 && score >= 60) {
-            System.out.println("C");
-        } else if (score < 60 && score >= 50) {
-            System.out.println("D");
+        if (score > 100) {
+            System.out.println("Error : Value must be less than or equal to 100.");
+        } else if (score < 0) {
+            System.out.println("Error : Value must be greater than or equal to 0.");
         } else {
-            System.out.println("F");
+            if (score >= 80) {
+                System.out.println("A");
+            } else if (score < 80 && score >= 70) {
+                System.out.println("B");
+            } else if (score < 70 && score >= 60) {
+                System.out.println("C");
+            } else if (score < 60 && score >= 50) {
+                System.out.println("D");
+            } else {
+                System.out.println("F");
+            }
         }
     }
 }
